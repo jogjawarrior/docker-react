f8b1e0d6880bd39bb1c6f86beefae205ea6dec22
Ignatius Richard Sutiono <richardsutiono12@gmail.com>
2022-11-25 14:13:08 +0700

    Change text on App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> .
+          {/* Edit <code>src/App.js</code> . */}
+          I was changed from features branch
         </p>
         <a
           className="App-link"
